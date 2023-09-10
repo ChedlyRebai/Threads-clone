1385cf5572d71eaf3da802507cadef34eedf45c6
chedly <chedly.rebai123@gmail.com>
2023-09-10 12:25:59 +0100

    delete

--- app/organization-switcher/[[...organization-switcher]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ClerkProvider, OrganizationSwitcher } from "@clerk/nextjs";
-
-export default function OrganizationSwitcherPage() {
-  return (
-    <div>
-      <OrganizationSwitcher />
-    </div>
-  );
-}
